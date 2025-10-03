ee8d72a35c682c932e287794eeb0d25d8cde599c
CapybaraSwim <ilya-golovin-03@mail.ru>
2025-10-03 15:34:30 +0300

    ouch

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const API_BASE_URL = 'http://109.73.206.144:6969'
+
 const apiClient = axios.create({
-  baseURL: '/api'
+    baseURL: API_BASE_URL,
+    params: {
+        key: 'E6kUTYrYwZq2tN4QEtyzsbEBk3ie'
+    }
 })
 
 export default apiClient
