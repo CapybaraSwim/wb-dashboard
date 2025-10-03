a1b8a5123bfb2d78f0a40c7f019665f732b9786f
CapybaraSwim <ilya-golovin-03@mail.ru>
2025-10-03 15:07:12 +0300

    add worker proxy

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,12 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://109.73.206.144:6969'
-
 const apiClient = axios.create({
-    baseURL: API_BASE_URL,
-    params: {
-        key: 'E6kUTYrYwZq2tN4QEtyzsbEBk3ie'
-    }
+  baseURL: '/api',
 })
 
 export default apiClient
